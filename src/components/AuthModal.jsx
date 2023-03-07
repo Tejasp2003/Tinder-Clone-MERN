04c939e3b3034f8fe642b3050cbed882857909d6
Tejasp2003 <tejasp200320@gmail.com>
2023-03-07 21:38:10 +0530

    completed till onBoarding UI

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AuthModal = ({ setShowModal }) => {
+const AuthModal = ({ setShowModal, isSignUp }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  
   const handleClick = () => {
     setShowModal(false);
   };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    try {
+      if (password !== confirmPassword) {
+        setError("Passwords do not match");
+      } else {
+        console.log("Success");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+
   return (
     <div className="auth-modal">
-      <div className="close-icon" onClick={handleClick}>
-        ⓧ
+      <div onClick={handleClick}>
+        <span className="close-icon">X</span>
       </div>
+      <h2>{isSignUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+      <p>
+        By clicking above button, you agree to our terms and condition. For more
+        details, you can deep dive into our privacy policy page
+      </p>
+
+      <form onSubmit={submitHandler}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required={true}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required={true}
+        />
+
+        {isSignUp && <input
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required={true}
+        />}
+        <input className="secondary-button" type="submit" />
+        <p>{error}</p>
+      </form>
+
+      <hr />
+      <h2>GET THE APP</h2>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,30 +2,32 @@ import React, { useState } from "react";
 import Nav from "../components/Nav";
 import AuthModal from "../components/AuthModal";
 
-
-
-
 const Home = () => {
-    const authToken = false;
-    const [showModal, setShowModal] = useState(false);
-    const handleClick = () => {
-        console.log("clicked");
-        setShowModal(true);
-      };
+  const authToken = false;
+  const [showModal, setShowModal] = useState(false);
+  const [isSignUp, setIsSignUp] = useState(true);
+  const handleClick = () => {
+    console.log("clicked");
+    setShowModal(true);
+    setIsSignUp(true);
+  };
   return (
     <>
       <div className="overlay">
-        <Nav minimal={false} authToken={authToken} setShowModal={setShowModal} showModal={showModal}/>
+        <Nav
+          minimal={false}
+          authToken={authToken}
+          setShowModal={setShowModal}
+          showModal={showModal}
+          setIsSignUp={setIsSignUp}
+        />
         <div className="home">
-          <h1>Swipe Right</h1>
+          <h1 className="primary-title">Swipe Right</h1>
           <button className="primary-button" onClick={handleClick}>
             {authToken ? "Signout" : "Create Account"}
           </button>
 
-            {showModal && <AuthModal 
-            
-            setShowModal={setShowModal}
-            />}
+          {showModal && <AuthModal setShowModal={setShowModal} setIsSignUp={setIsSignUp} isSignUp={isSignUp}/>}
         </div>
       </div>
     </>
